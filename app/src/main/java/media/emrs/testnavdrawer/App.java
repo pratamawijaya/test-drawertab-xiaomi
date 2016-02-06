1b3a8c808b305d3757fe06eecdd55ab8d0c1a422
pratamawijaya <set.mnemonix@gmail.com>
2016-02-06 14:11:18 +0700

    add fabric

--- app/src/main/java/media/emrs/testnavdrawer/App.java
+++ app/src/main/java/media/emrs/testnavdrawer/App.java
@@ -1,6 +1,8 @@
 package media.emrs.testnavdrawer;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by : pratama - set.mnemonix@gmail.com
@@ -10,6 +12,7 @@ import android.app.Application;
 public class App extends Application {
   @Override public void onCreate() {
     super.onCreate();
+    Fabric.with(this, new Crashlytics());
 
   }
 }
